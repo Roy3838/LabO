359ad80234cb11c1b0ac1e1a19b39c0c49e3186e
Bajtsy <a01570628@tec.mx>
2023-03-10 09:09:00 -0600

    Imagenes del viernes

--- Reto/Noise_Suppression.jl
+++ Reto/Noise_Suppression.jl
@@ -7,7 +7,7 @@ using DSP
 
 #Import Images
 ref_path_0 = load("C:/Documentos/Studying/Clases/6to Semestre/LabOptica/LabO/pikas/Fotos_jueves_9/DSC_0006_finas_ref.JPG")
-img_path_0 = load("C:/Documentos/Studying/Clases/6to Semestre/LabOptica/LabO/pikas/Fotos_jueves_9/DSC_0005_finas_135.JPG")
+img_path_0 = load("C:/Documentos/Studying/Clases/6to Semestre/LabOptica/LabO/pikas/Fotos_Viernes_10_2/DSC_0026.JPG")
 
 ref_path_0[470:2450,1500:3000]
 #GrayScale and chop to effective Image
@@ -16,20 +16,25 @@ matrix_ref_0 = Float32.(gray_ref_0)
 matrix_ref_0 = matrix_ref_0.-[minimum(matrix_ref_0)]
 matrix_ref_0 = matrix_ref_0./maximum(matrix_ref_0)
 
-gray_img_0 = Gray.(img_path_0[800:2000,1500:3000])
+gray_img_0 = Gray.(img_path_0[1500:3300,2000:3800])
 matrix_img_0 = Float32.(gray_img_0)
 matrix_img_0 = matrix_img_0.-[minimum(matrix_img_0)]
 matrix_img_0 = matrix_img_0./maximum(matrix_img_0)
 
+xlen = length(matrix_img_0[1050,:])
+ylen = length(matrix_img_0[:,1])
 
-
-"sze =2
+x = 1:xlen
+y = 1:ylen
+plot(x, matrix_img_0[1050,:])
+matrix_img_0[1050,:]
+sze = 4
 
 noissupp = 1/(sze^2) * ones(2*sze+1,2*sze+1)
 nois_mat = zeros(ylen,xlen)
 for ii in range(sze+1, ylen-sze-1)
     for jj in range(sze+1, xlen-sze-1)
-        nois_mat[ii, jj] = sum(noissupp.*gray_matrix[ii-sze:ii+sze, jj-sze:jj+sze])
+        nois_mat[ii, jj] = sum(noissupp.*matrix_img_0[ii-sze:ii+sze, jj-sze:jj+sze])
     end
 end
 nois_mat = nois_mat./[maximum(nois_mat)]
@@ -39,7 +44,7 @@ plot(x[sze+1:xlen-sze-1], y[sze+1:ylen-sze-1],
         st = :surface, camera = (0,90))
 xut = x[sze+1:xlen-sze-1]
 xlenut = length(xut)
-transvcut = gray_matrix[600,sze+1:xlen-sze-1] 
+transvcut = nois_mat[1050,sze+1:xlen-sze-1] 
 
 # Decrease transvcut by 0.7
 transvcutn = nois_mat[600,sze+1:xlen-sze-1]
