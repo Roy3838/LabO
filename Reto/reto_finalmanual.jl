7a3bb9fb79b9e7493d104fa3a681fc9f54ae7961
Roy3838 <a01283783@itesm.com>
2023-03-15 12:47:30 -0600

    Mach-Zehnder

--- Reto/reto_finalmanual.jl
+++ Reto/reto_finalmanual.jl
@@ -4,51 +4,52 @@ using DSP
 
 # --------------PARAMETERS------------#
 
-height = 1800
-width = 1700
-IM_PATH = "C:/Users/JayPC/LabO/pikas/Fotos_Viernes_10_2/"
+height = 1000
+width = 1000
+IM_PATH = "/Users/roymedina/LabO/pikas/Karla/"
 
 # Crop parameters
-x1_ref=1501
-x2_ref=3300
-y1_ref=2101
-y2_ref=3800
+y1_ref=1800
+y2_ref=2800
+x1_ref=1000
+x2_ref=2000
 
-x1_im = x1_ref - 60  # el vato alineando las fotos
+x1_im = x1_ref  
 x2_im = x2_ref 
-y1_im = y1_ref + 75  
-y2_im = y2_ref + 150 # alv che gil chochado
+y1_im = y1_ref   
+y2_im = y2_ref
 
 # -----------IMPORT IMAGES-----------#
-ref1 = Float64.(Gray.(load(IM_PATH*"DSC_0026.JPG")))
-ref2 = Float64.(Gray.(load(IM_PATH*"DSC_0027.JPG")))
-ref3 = Float64.(Gray.(load(IM_PATH*"DSC_0028.JPG")))
-ref4 = Float64.(Gray.(load(IM_PATH*"DSC_0029.JPG")))
+ref1 = Float64.(Gray.(load(IM_PATH*"Referencia 1.JPG")))
+ref2 = Float64.(Gray.(load(IM_PATH*"Referencia 2.JPG")))
+ref3 = Float64.(Gray.(load(IM_PATH*"Referencia 3.JPG")))
+ref4 = Float64.(Gray.(load(IM_PATH*"Referencia 4.JPG")))
 
 # ---------IMPORT REFERENCES--------#
 
-im1 = Float64.(Gray.(load(IM_PATH*"DSC_0033.JPG")))
-im2 = Float64.(Gray.(load(IM_PATH*"DSC_0032.JPG")))
-im3 = Float64.(Gray.(load(IM_PATH*"DSC_0031.JPG")))
-im4 = Float64.(Gray.(load(IM_PATH*"DSC_0030.JPG")))
+im1 = Float64.(Gray.(load(IM_PATH*"Franjas 1.JPG")))
+im2 = Float64.(Gray.(load(IM_PATH*"Franjas 2.JPG")))
+im3 = Float64.(Gray.(load(IM_PATH*"Franjas 3.JPG")))
+im4 = Float64.(Gray.(load(IM_PATH*"Franjas 4.JPG")))
 
-# -----------CROP IMAGES------------#
+
+# # -----------CROP IMAGES------------#
 include("crop.jl")
 im1 = crop(im1,x1_im,x2_im,y1_im,y2_im)
 im2 = crop(im2,x1_im,x2_im,y1_im,y2_im)
 im3 = crop(im3,x1_im,x2_im,y1_im,y2_im)
 im4 = crop(im4,x1_im,x2_im,y1_im,y2_im)
 
-# -----------CROP REFERENCES------------#
+# # -----------CROP REFERENCES------------#
 ref1 = crop(ref1,x1_ref,x2_ref,y1_ref,y2_ref)
 ref2 = crop(ref2,x1_ref,x2_ref,y1_ref,y2_ref)
 ref3 = crop(ref3,x1_ref,x2_ref,y1_ref,y2_ref)
 ref4 = crop(ref4,x1_ref,x2_ref,y1_ref,y2_ref)
 
-im1 = imresize(im1, (height, width))
-im2 = imresize(im2, (height, width))
-im3 = imresize(im3, (height, width))
-im4 = imresize(im4, (height, width))
+# im1 = imresize(im1, (height, width))
+# im2 = imresize(im2, (height, width))
+# im3 = imresize(im3, (height, width))
+# im4 = imresize(im4, (height, width))
 
 include("noise_supp.jl")
 
@@ -65,18 +66,10 @@ ref2_n = noise_supp(ref2, sze)
 ref3_n = noise_supp(ref3, sze)
 ref4_n = noise_supp(ref4, sze)
 
+heatmap(im1_n)
+
 #Crop lost information
-crp = 100
 
-im1_n = im1_n[crp:end-crp, crp:end-crp]
-im2_n = im2_n[crp:end-crp, crp:end-crp]
-im3_n = im3_n[crp:end-crp, crp:end-crp]
-im4_n = im4_n[crp:end-crp, crp:end-crp]
-
-ref1_n = ref1_n[crp:end-crp, crp:end-crp]
-ref2_n = ref2_n[crp:end-crp, crp:end-crp]
-ref3_n = ref3_n[crp:end-crp, crp:end-crp]
-ref4_n = ref4_n[crp:end-crp, crp:end-crp]
 
 Φi = atan.(-im2_n+im4_n,im1_n-im3_n)
 
@@ -84,9 +77,11 @@ ref4_n = ref4_n[crp:end-crp, crp:end-crp]
 
 include("unwrap_chochado.jl")
 
-#Φu = unwrap(Φs-Φi,dims = 1:2,range = 2pi)
 Φu = Φs-Φi
-Φu = Φu[1:1500, 1:1500]
+
+#Φu = unwrap(Φu,dims = 1:2,range = 2pi)
+
+#heatmap(Φu)
 
 psi = Φu
 
@@ -105,35 +100,35 @@ dctPhi = dctRho ./ 2 ./ (cos(pi*I/M) + cos(pi*J/N) .- 2)
 dctPhi[1,1] = 0 # handling the inf/nan value
 phi = mapslices(idct, dctPhi, dims=(1,2))
 
-Φu = unwrap(Φu, dims = 1:2,range = 2pi)
+# Φu = unwrap(Φu, dims = 1:2,range = 2pi)
 
 plotlyjs()
-surface(Φu, color = :thermal, showaxis = false, camera = (30, 30), title = "Phase")
+surface(phi, color = :thermal, showaxis = false, camera = (30, 30), title = "Phase")
 
 
-# θ = atan(18/39.5)
+# # θ = atan(18/39.5)
 
-# Φu_n = noise_supp(Φu, 2)
-# h = 1:length(Φu_n[:,1])
-# w = 1:length(Φu_n[1,:])
+# # Φu_n = noise_supp(Φu, 2)
+# # h = 1:length(Φu_n[:,1])
+# # w = 1:length(Φu_n[1,:])
 
-# # -------------------MESHGRID FUNCTION------------------#
-# function meshgrid(xin,yin)
-#     nx=length(xin)
-#     ny=length(yin)
-#     xout=zeros(ny,nx)
-#     yout=zeros(ny,nx)
-#     for jx=1:nx
-#         for ix=1:ny
-#             xout[ix,jx]=xin[jx]
-#             yout[ix,jx]=yin[ix]
-#         end
-#     end
-#     return (x=xout, y=yout)
-# end
-# x_l, y_l = meshgrid(w, h)
+# # # -------------------MESHGRID FUNCTION------------------#
+# # function meshgrid(xin,yin)
+# #     nx=length(xin)
+# #     ny=length(yin)
+# #     xout=zeros(ny,nx)
+# #     yout=zeros(ny,nx)
+# #     for jx=1:nx
+# #         for ix=1:ny
+# #             xout[ix,jx]=xin[jx]
+# #             yout[ix,jx]=yin[ix]
+# #         end
+# #     end
+# #     return (x=xout, y=yout)
+# # end
+# # x_l, y_l = meshgrid(w, h)
 
-# surface(w, h, abs.(Φu_n))
+# # surface(w, h, abs.(Φu_n))
 
 
 
