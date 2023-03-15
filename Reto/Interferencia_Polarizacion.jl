bb27bde4eba6502228a22e46d5b7d809823c097f
Bajtsy <a01570628@tec.mx>
2023-03-15 14:56:39 -0600

    x2

--- Reto/Interferencia_Polarizacion.jl
+++ Reto/Interferencia_Polarizacion.jl
@@ -17,11 +17,11 @@ ys = ymax*(2/Ntot)*(-Ntot/2:Ntot/2-1)
 #Meshgrid
 Ys = -ones(length(xs)).*xs*ones(length(xs))'
 Xs = -Ys'
-z = 10;
+z = 1;
 
 # Vector de propagacion
 k = 2*pi/lambda
-thz = pi/6000
+thz = pi/40000
 thxy = 0
 kx = k*sin(thz)*cos(thxy)
 ky = k*sin(thz)*sin(thxy)
@@ -33,28 +33,40 @@ E1 = E0.*exp.(1im.*(kx.*Xs .+ ky.*Ys .+ kz.*z)).*[[1; 0]]
 E2 = E0.*exp.(1im.*(-kx.*Xs .+ ky.*Ys .+ kz.*z)).*[[0; 1]]
 Et = E1 + E2
 
+yc = 1:120
+xc = 1:160
+Etc = Et[yc,xc]
 #Visualizamos el campo y su polarización
-heatmap(norm.(Et).^2,clim = (0,2), label = false, c = :grays)
-
 N = 8
-for ii in range(1,Int(Ntot/N))
-    yr = N*ii-(N-1):N*ii
-    for jj in range(1,Int(Ntot/N))
-        xr = N*jj-(N-1):N*jj
+heightc = length(Etc[:,1])
+widthc = length(Etc[1,:])
+wc = 1:widthc
+hc = 1:heightc
+#heatmap(wc, hc, I0c, c = :grays, )
+elp = palette([:red, :blue], 201);
+heatmap(norm.(Etc).^2, c = :gray, clim = (0,2))
+prom
 
-        prom = mean(Et[yr, xr])
-        Ix = norm.([1 0]*prom).^2
-        Iy = norm.([0 1]*prom).^2
-        Ir = norm.([1 1im]*prom).^2
-        Il = norm.([1 -1im]*prom).^2
-        Id = norm.([1 1]*prom).^2
-        Ia = norm.([1 -1]*prom).^2
+prom = Etc
+Ix = norm.([[1 0]].*prom).^2
+Iy = norm.([[0 1]].*prom).^2
+Ir = norm.([[1 1im]].*prom).^2
+Il = norm.([[1 -1im]].*prom).^2
+Id = norm.([[1 1]].*prom).^2
+Ia = norm.([[1 -1]].*prom).^2
+maxS2 = maximum(Ir-Il)
 
-        S0_p = Ix[1] + Iy[1]
-        S1_p = Ix[1] - Iy[1]
-        S2_p = Ir[1] - Il[1]
-        S3_p = Id[1] - Ia[1]
+for ii in range(1,Int(heightc/N))
+    yr = Int(N/2)*ii
+    for jj in range(1,Int(widthc/N))
+        xr = Int(N/2)*jj
 
+        S0_p = Ix[yr, xr] + Iy[yr, xr]
+        S1_p = Ix[yr, xr] - Iy[yr, xr]
+        S2_p = Ir[yr, xr] - Il[yr, xr]
+        S3_p = Id[yr, xr] - Ia[yr, xr]
+
+        clr = Int(round(round(S2_p/maxS2, digits = 2)*100+101))
         χ = (1/2)*atan.(S3_p, S1_p)
         b = sqrt.((1 - atan.((S1_p.^2+S3_p.^2),S2_p.^2)./(pi/2))/2)
         a = sqrt(1 - b.^2)
@@ -67,9 +79,9 @@ for ii in range(1,Int(Ntot/N))
         y = a*cos.(theta).*sin(χ) + b*sin.(theta).*cos(χ)
         
         # Scale into square size (N*N) and positionate
-        xs = (N/2).*x .+ xr[Integer(round(end/2))]
-        ys = (N/2).*y.+ yr[Integer(round(end/2))]
-        plot!(xs, ys, lc = :blue, lw = 2, label = false)
+        xs = (N/2).*x .+ 2*xr.-(N/2)
+        ys = (N/2).*y.+ 2*yr.-(N/2)
+        plot!(xs, ys, lc = elp[clr], lw = 2, label = false)
     end
 end
 plot!(xs, ys, lc = :blue, lw = 2, label = false)
@@ -81,28 +93,29 @@ function retλ4(θ)
     return rλ4
 end;
 Interf = retλ4(pi/4)
-Etr4 = [Interf].*Et
+Etr4 = [Interf].*Etc
 heatmap((norm.(Etr4).^2), clim = (0,2), c = :grays)
 
 N = 8
-for ii in range(1,Int(Ntot/N))
-    yr = N*ii-(N-1):N*ii
-    for jj in range(1,Int(Ntot/N))
-        xr = N*jj-(N-1):N*jj
 
-        prom = mean(Etr4[yr, xr])
-        Ix = norm.([1 0]*prom).^2
-        Iy = norm.([0 1]*prom).^2
-        Ir = norm.([1 1im]*prom).^2
-        Il = norm.([1 -1im]*prom).^2
-        Id = norm.([1 1]*prom).^2
-        Ia = norm.([1 -1]*prom).^2
+prom = Etr4
+Ix = norm.([[1 0]].*prom).^2
+Iy = norm.([[0 1]].*prom).^2
+Ir = norm.([[1 1im]].*prom).^2
+Il = norm.([[1 -1im]].*prom).^2
+Id = norm.([[1 1]].*prom).^2
+Ia = norm.([[1 -1]].*prom).^2
+maxS2 = maximum(Ir-Il)
+for ii in range(1,Int(heightc/N))
+    yr = Int(N/2)*ii
+    for jj in range(1,Int(widthc/N))
+        xr = Int(N/2)*jj
 
-        S0_p = Ix[1] + Iy[1]
-        S1_p = Ix[1] - Iy[1]
-        S2_p = Ir[1] - Il[1]
-        S3_p = Id[1] - Ia[1]
-
+        S0_p = Ix[yr, xr] + Iy[yr, xr]
+        S1_p = Ix[yr, xr] - Iy[yr, xr]
+        S2_p = Ir[yr, xr] - Il[yr, xr]
+        S3_p = Id[yr, xr] - Ia[yr, xr]
+        clr = 101
         χ = (1/2)*atan.(S3_p, S1_p)
         b = sqrt.((1 - atan.((S1_p.^2+S3_p.^2),S2_p.^2)./(pi/2))/2)
         a = sqrt(1 - b.^2)
@@ -115,9 +128,9 @@ for ii in range(1,Int(Ntot/N))
         y = a*cos.(theta).*sin(χ) + b*sin.(theta).*cos(χ)
         
         # Scale into square size (N*N) and positionate
-        xs = (N/2).*x .+ xr[Integer(round(end/2))]
-        ys = (N/2).*y.+ yr[Integer(round(end/2))]
-        plot!(xs, ys, lc = :blue, lw = 2, label = false)
+        xs = (N/2).*x .+ 2*xr.-(N/2)
+        ys = (N/2).*y.+ 2*yr.-(N/2)
+        plot!(xs, ys, lc = elp[clr], lw = 2, label = false)
     end
 end
 plot!(xs, ys, lc = :blue, lw = 2, label = false)
@@ -133,24 +146,24 @@ Interfpol = Jpol(pi/2)
 Etfin = [Interfpol].*Etr4
 heatmap((norm.(Etfin).^2), c = :grays)
 N = 8
-for ii in range(1,Int(Ntot/N))
-    yr = N*ii-(N-1):N*ii
-    for jj in range(1,Int(Ntot/N))
-        xr = N*jj-(N-1):N*jj
+prom = Etfin
+Ix = norm.([[1 0]].*prom).^2
+Iy = norm.([[0 1]].*prom).^2
+Ir = norm.([[1 1im]].*prom).^2
+Il = norm.([[1 -1im]].*prom).^2
+Id = norm.([[1 1]].*prom).^2
+Ia = norm.([[1 -1]].*prom).^2
+maxS2 = maximum(Ir-Il)
+for ii in range(1,Int(heightc/N))
+    yr = Int(N/2)*ii
+    for jj in range(1,Int(widthc/N))
+        xr = Int(N/2)*jj
 
-        prom = mean(Etfin[yr, xr])
-        Ix = norm.([1 0]*prom).^2
-        Iy = norm.([0 1]*prom).^2
-        Ir = norm.([1 1im]*prom).^2
-        Il = norm.([1 -1im]*prom).^2
-        Id = norm.([1 1]*prom).^2
-        Ia = norm.([1 -1]*prom).^2
-
-        S0_p = Ix[1] + Iy[1]
-        S1_p = Ix[1] - Iy[1]
-        S2_p = Ir[1] - Il[1]
-        S3_p = Id[1] - Ia[1]
-
+        S0_p = Ix[yr, xr] + Iy[yr, xr]
+        S1_p = Ix[yr, xr] - Iy[yr, xr]
+        S2_p = Ir[yr, xr] - Il[yr, xr]
+        S3_p = Id[yr, xr] - Ia[yr, xr]
+        clr = 101
         χ = (1/2)*atan.(S3_p, S1_p)
         b = sqrt.((1 - atan.((S1_p.^2+S3_p.^2),S2_p.^2)./(pi/2))/2)
         a = sqrt(1 - b.^2)
@@ -163,9 +176,9 @@ for ii in range(1,Int(Ntot/N))
         y = a*cos.(theta).*sin(χ) + b*sin.(theta).*cos(χ)
         
         # Scale into square size (N*N) and positionate
-        xs = (N/2).*x .+ xr[Integer(round(end/2))]
-        ys = (N/2).*y.+ yr[Integer(round(end/2))]
-        plot!(xs, ys, lc = :blue, lw = 2, label = false)
+        xs = (N/2).*x .+ 2*xr.-(N/2)
+        ys = (N/2).*y.+ 2*yr.-(N/2)
+        plot!(xs, ys, lc = elp[clr], lw = 2, label = false)
     end
 end
 plot!(xs, ys, lc = :blue, lw = 2, label = false)
