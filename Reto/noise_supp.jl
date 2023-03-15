553e26c2c1ab48623a2d7da1a17b0b799d1755ae
Bajtsy <a01570628@tec.mx>
2023-03-14 20:08:20 -0600

    -

--- Reto/noise_supp.jl
+++ Reto/noise_supp.jl
@@ -1,18 +1,11 @@
 # ------------NOISE SUPPRESSION----------#
-
+using ImageFiltering
 function noise_supp(img::Array{Float64,2}, sze::Int)::Array{Float64,2}
 
-    noissupp = 1/((2*sze+1)^2) * ones(2*sze+1,2*sze+1)
-    h = length(img[:,1])
-    w = length(img[1,:])
-    nois_mat = zeros(h,w)
+    kern1 = 1/(2*sze+1) * ones(1,2*sze+1)
+    kernf = kernelfactors((kern1, kern1))
+    img = imfilter(img, kernf)
 
-    for ii in range(sze+1, h-sze-1)
-        for jj in range(sze+1, w-sze-1)
-            nois_mat[ii, jj] = sum(noissupp.*img[ii-sze:ii+sze, jj-sze:jj+sze])
-        end
-    end
-
-    return nois_mat
+    return img
 
 end
