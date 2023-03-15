7d517efe4138361f9d911a7e4362c068ab1ceec2
Bajtsy <a01570628@tec.mx>
2023-03-15 14:56:26 -0600

    Correccion de interferencia_polarizacion

--- Reto/stokes._ret4.jl
+++ Reto/stokes._ret4.jl
@@ -12,20 +12,20 @@ width = 640
 h = 1:height
 w = 1:width
 
-gil_path = "C:/Documentos/Studying/Clases/6to Semestre/LabOptica/LabO/pikas/Fotos_Miercoles_Stokes/"
+gil_path = "C:/Documentos/Studying/Clases/6to Semestre/LabOptica/LabO/pikas/Fotos_Mieroles_Stokes_polarizador/"
 roy_path = "/Users/roymedina/LabO/pikas/Stokes/"
 IM_PATH = gil_path
 
 
 #--------------------IMPORT IMAGES -------------#
 
-I0 = Float64.(Gray.(load(IM_PATH*"WIN_20230315_09_25_49_Pro_I0.jpg")))
-Ix = Float64.(Gray.(load(IM_PATH*"WIN_20230315_09_11_07_Pro_Horizontal.jpg")))
-Iy = Float64.(Gray.(load(IM_PATH*"WIN_20230315_09_11_56_Pro_Vertical.jpg")))
-Id = Float64.(Gray.(load(IM_PATH*"WIN_20230315_09_11_37_Pro_Diagonal.jpg")))
-Ia = Float64.(Gray.(load(IM_PATH*"WIN_20230315_09_12_12_Pro_Antidiagonal.jpg")))
-Ir = Float64.(Gray.(load(IM_PATH*"WIN_20230315_09_15_41_Pro_Derecha.jpg")))
-Il = Float64.(Gray.(load(IM_PATH*"WIN_20230315_09_16_04_Pro_Izquierda.jpg")))
+I0 = Float64.(Gray.(load(IM_PATH*"WIN_20230315_09_43_58_Pro_Polarizador_I0.jpg")))
+Ix = Float64.(Gray.(load(IM_PATH*"WIN_20230315_09_37_30_Pro_Polarizador_Horizontal.jpg")))
+Iy = Float64.(Gray.(load(IM_PATH*"WIN_20230315_09_37_56_Pro_Polarizador_Vertcal.jpg")))
+Id = Float64.(Gray.(load(IM_PATH*"WIN_20230315_09_37_39_Pro_Polarizador_Diagonal.jpg")))
+Ia = Float64.(Gray.(load(IM_PATH*"WIN_20230315_09_38_12_Pro_Polarizador_Antidiagonal.jpg")))
+Ir = Float64.(Gray.(load(IM_PATH*"WIN_20230315_09_39_20_Pro_Polarizador_Derecha.jpg")))
+Il = Float64.(Gray.(load(IM_PATH*"WIN_20230315_09_39_48_Pro_Polarizador_Izquierda.jpg")))
 #---------------STOKES PARAMETERS----------------#
 
 S0 = Iy + Ix
@@ -39,7 +39,6 @@ S3 = 0
 S2 = √(S0^2-S1^2-S3^2)"
 
 #Number of polarization elpises
-
 
 #----------------Crop-----------------
 yc = 181:300
@@ -56,19 +55,18 @@ widthc = length(I0c[1,:])
 wc = 1:widthc
 hc = 1:heightc
 #heatmap(wc, hc, I0c, c = :grays, )
-elp = palette([:red, :blue], 201);
-heatmap(0, c = elp)
+plot(0,0)
 for ii in range(1,Int(heightc/N))
     yr = Int(N/2)*ii
     for jj in range(1,Int(widthc/N))
         xr = Int(N/2)*jj
+
 
         S0_p = S0c[yr,xr]
         S1_p = S1c[yr,xr]
         S2_p = S2c[yr,xr]
         S3_p = S3c[yr,xr]
 
-        clr = Int(round(round(S2_p/maximum(S2c), digits = 2)*100+101))
         χ = (1/2)*atan.(S3_p, S1_p)
         b = sqrt.((1 - atan.((S1_p.^2+S3_p.^2),S2_p.^2)./(pi/2))/2)
         a = sqrt(1 - b.^2)
@@ -83,28 +81,13 @@ for ii in range(1,Int(heightc/N))
         # Scale into square size (N*N) and positionate
         xs = (N/2).*x .+ 2*xr.-(N/2)
         ys = (N/2).*y.+ 2*yr.-(N/2)
-        plot!(xs, ys, lc = elp[clr], lw = 2, label = false)
+        plot!(xs, ys, lc = :blue, lw = 2, label = false)
     end
 end
 
-plot!(0, 0, lc = :blue, lw = 2, label = false, color= elp )
-cbar = colorbar!()
+plot!(0, 0, lc = :blue, lw = 2, label = false)
 
 xlabel!("x")
 ylabel!("y")
 
 savefig(IM_PATH"Polarizacion_Exp.jpg")
-
-using Plots
-using ColorSchemes
-
-# Create some sample data
-x = 1:10
-y = rand(10, 5)
-
-# Plot the data with different colors
-p = plot(x, y, color=[:blue :red :green :orange :purple], linewidth=2, label=["A" "B" "C" "D" "E"])
-
-# Add a colorbar to the plot
-cbar = Colorbar(p, label="Legend")
-
