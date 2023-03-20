fc82f46321ae9ef335d7120e58db3af57cd331c9
Bajtsy <a01570628@tec.mx>
2023-03-20 09:34:20 -0600

    correcciones

--- Reto/GraficaDesenvFase.jl
+++ Reto/GraficaDesenvFase.jl
@@ -6,7 +6,7 @@ theta = 0:0.05:pi
 y = (1/2)*atan.(tan.(2*theta))
 plot(theta, y, lc = :blue, lw = 2, grid = :true, 
 gridalpha = 0.75, framestyle = :origin, label = "Fase", legendfont = font(10))
-plot!(theta, theta, lc = :red, lw = 2, grid = :true, alpha = 0.5,
+    plot!(theta, theta, lc = :red, lw = 2, grid = :true, alpha = 0.5,
 gridalpha = 0.3, framestyle = :origin, label = "Fase Desenvuelta", legendfont = font(10),
 xguidefontsize = 16, yguidefontsize = 16)
 xlabel!(L"\theta")
