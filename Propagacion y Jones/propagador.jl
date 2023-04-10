eb7aebfbe0e0ee2f86cd632f046ef65cef646e3b
Roy3838 <a01283783@itesm.mx>
2023-04-09 18:58:19 -0600

    Correcion a propagador de fourier

--- Propagacion y Jones/propagador.jl
+++ Propagacion y Jones/propagador.jl
@@ -2,14 +2,19 @@ using Plots
 using FFTW
 
 # Codigo para hacer un propagador de Fourier 
-# donde se encunetra un U_0 y se propaga una distancia z
+# donde se encuentra un U_0 y se propaga una distancia z
+function meshgrid(x, y)
+    X = repeat(x', length(y), 1)
+    Y = repeat(y, 1, length(x))
+    return X, Y
+end
 
 
 # Usando propagacion de Fourier
-function propagar(U_0, z, k_x, k_y)
-
+function propagar(U_0, z, KXs, KYs, k_0)
     F_U = fft(U_0) # Transformada de Fourier de U_0
-    F_Um = exp.(im.*k_x.*k_y.*z).*F_U # Propagacion de Fourier
+    kz = k_0 .- 0.5 .* (KXs.^2 .+ KYs.^2) ./ k_0
+    F_Um = exp.(1im .* kz .* z) .* F_U # Propagacion de Fourier
     U = ifft(F_Um) # Transformada inversa de Fourier
     return U
 end
@@ -19,7 +24,7 @@ N = 512
 U_0 = zeros(Complex{Float64},N,N)
 
 # Definir variables de propagacion
-z = 0.000000000003 # propagation distance
+z = 4 # propagation distance
 
 lambda = 633e-9
 w0 = 0.5e-3
@@ -31,18 +36,13 @@ ys = NV*dx
 Xs = xs'.*ones(N)
 Ys = ys.*ones(N)
 k_0 = 2*pi/lambda
-# print shape of Xs
-print(size(Xs))
 
 k_max = pi/dx
-
 k_x = k_max * (2/N) * NV
 k_y = k_max * (2/N) * NV
 
-#[KXs, KYs] = meshgrid(k_x, k_y)
+KXs, KYs = meshgrid(k_x, k_y)
 
-# propagador
-#kz = k_0-0.5(KXs.^2 + KYs.^2)/k_0
 zr = k_0*(w0^2)/2
 z = 0.5*zr
 nz = 300
@@ -61,15 +61,9 @@ end
 # Visualizar campo inicial
 #heatmap(abs.(U_0).^2)
 
+U = propagar(U_0, z, KXs, KYs, k_0)
+U = propagar(U, 4*z, KXs, KYs, k_0)
 
-
-
-U = propagar(U_0, z, k_x, k_y)
 
 
 heatmap(abs.(U).^2)
-
-# Propagar
-
-#heatmap(abs.(U).^2)
-
